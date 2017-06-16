482da2c0059cd63ea7aa31b84809850ceb5c67df
zhanghao <hao.zhang@roboterra.com>
2017-06-16 16:08:27 +0800

    gulp packet

---
+++ build/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by roboterra_rd on 2017/6/16.
+ */
+var h1='zhanghao234';
+function sayHello(data){console.log('hello1111'+h1+data);}
+var aa=11;var a=23242442244555556666;
+sayHello('是个大英雄1133334444');
+console.log(document.getElementsByClassName('dd')[0].innerHTML);
+
