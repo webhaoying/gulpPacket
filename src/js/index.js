f6d62d052659334b72d4553a64e97ae1e8b44199
webhaoying <zlyheut@163.com>
2017-06-16 16:27:22 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 /**
  * Created by roboterra_rd on 2017/6/16.
+ 
+ npm install --save-dev jshint gulp-jshint  gulp-less  gulp-rename gulp-sass  gulp-rename  gulp-minify-css gulp-uglify gulp-browserify gulp-connect
  */
 var h1='zhanghao234';
 function sayHello(data){console.log('hello1111'+h1+data);}
